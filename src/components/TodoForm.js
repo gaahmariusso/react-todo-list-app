5600e927ac893df38a625eb40352e33d44f1609b
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2022-07-12 22:18:24 -0300

    fix edit todo

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 function TodoForm(props) {
-    const [input, setInput] = useState('');
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
 
     const inputRef = useRef(null)
 
@@ -26,16 +26,33 @@ function TodoForm(props) {
 
     return (
         <form className='todo-form' onSubmit={handleSubmit}>
-            <input
-                type="text"
-                placeholder='Escreva sua tarefa'
-                value={input}
-                name='text'
-                className='todo-input'
-                onChange={handleChange}
-                ref={inputRef}
-            />
-            <button className='todo-button'>Adicionar</button>
+            {props.edit ? (
+                <>
+                    <input
+                        type="text"
+                        placeholder='Edite sua tarefa'
+                        value={input}
+                        name='text'
+                        className='todo-input edit'
+                        onChange={handleChange}
+                        ref={inputRef}
+                    />
+                    <button className='todo-button edit'>Adicionar</button>
+                </>
+            ) : (
+                <>
+                    <input
+                        type="text"
+                        placeholder='Escreva sua tarefa'
+                        value={input}
+                        name='text'
+                        className='todo-input'
+                        onChange={handleChange}
+                        ref={inputRef}
+                    />
+                    <button className='todo-button'>Adicionar</button>
+                </>
+            )}
         </form>
     );
 }
